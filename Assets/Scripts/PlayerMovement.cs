6bc22c15b28de9708b67ea4746136e3c5e8643ea
amkrosa <annakrasowska2@gmail.com>
2019-12-29 13:30:33 +0100

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody;
+    public Camera MainCamera;
+    public float Speed = 5;
+    public float RotationSpeed = 5f;
+    public bool OnlyKeyboard;
+
+    public Vector2 _move;
+    public Vector2 _mouse;
+    public float _angle;
+
+    private void Update()
+    {
+        if (OnlyKeyboard)
+        {
+            _angle = Input.GetAxis("Horizontal");
+            _move.y = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            _move.x = Input.GetAxis("Horizontal");
+            _move.y = Input.GetAxis("Vertical");
+            _mouse = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        Vector3 cameraPos = new Vector3(transform.position.x, transform.position.y, MainCamera.transform.position.z);
+        MainCamera.transform.position = cameraPos;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (OnlyKeyboard)
+        {
+            float angle = Rigidbody.rotation * Mathf.Deg2Rad;
+            _move.x = -_move.y * Mathf.Sin(angle);
+            _move.y = _move.y * Mathf.Cos(angle);
+            Rigidbody.MovePosition(Rigidbody.position + _move * Speed * Time.fixedDeltaTime);
+            Rigidbody.rotation -= _angle * RotationSpeed;
+        }
+        else
+        {
+            Rigidbody.MovePosition(Rigidbody.position + _move * Speed * Time.fixedDeltaTime);
+            Vector2 pointing = (_mouse - Rigidbody.position);
+            float angle = Mathf.Atan2(pointing.y, pointing.x) * Mathf.Rad2Deg - 90f;
+            Rigidbody.rotation = angle;
+        }
+    }
+
+    private void OnGUI()
+    {
+        OnlyKeyboard = GUILayout.Toggle(OnlyKeyboard, "OnlyKeyboard");
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public Transform BulletOrigin;
+    public float BulletForce;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = Instantiate(BulletPrefab, BulletOrigin.position, BulletOrigin.rotation);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            bulletRb.AddForce(BulletOrigin.up * BulletForce, ForceMode2D.Impulse);
+        }
+    }
+
+}
